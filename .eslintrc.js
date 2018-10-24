30147d6fd40126ef7334360a67a69b6e946aaa66
Anoop <anoop.dhiman12@gmail.com>
2018-10-24 12:38:00 +0530

    setup up store for Login Form

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
-module.exports = {
-    "extends": "standard"
+export default {
+    "ecmaFeatures": {
+        "blockBindings": true,
+        "superInFunctions": true
+      },
+      "env": {
+        "es6": true,
+        "node": true
+      },
+      "rules": {
+        "quotes": [2, "single"]
+      }
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux' 
+import { emailChanged } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
 
+
+
 class LoginForm extends Component {
+  onEmailChange(text){
+    this.props.emailChanged(text)
+  }
   render () {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" plageholder="user@email.com"></Input>
+          <Input label="Email" plageholder="user@email.com" onChangeText={this.onEmailChange.bind(this)}></Input>
         </CardSection>
         <CardSection>
           <Input secureTextEntry label="Password" plageholder="Password"></Input>
@@ -19,4 +26,6 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm
+
+
+export default connect(null,emailChanged )(LoginForm);
