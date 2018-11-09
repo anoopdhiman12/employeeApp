b627b5870af6664805f099f9ce46691b62d568f6
Anoop <anoop.dhiman12@gmail.com>
2018-11-09 23:52:40 +0530

    employee list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,51 +1,54 @@
-import _ from 'lodash'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { ListView,Text } from 'react-native'
-import { employeesFetch } from '../actions'
-import ListItem from './ListItem'
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
- class EmployeeList extends Component {
-     componentWillMount() {
-        this.props.employeesFetch()
-        this.createDataSource(this.props)
-     }
+class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
 
-     componentWillReceiveProps(nextProps){
-         this.createDataSource(nextProps)
-     }
+    this.createDataSource(this.props);
+  }
 
-     createDataSource({ employees}){
-        
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1,r2) => r1 != r2
-        })
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
 
-        this.DataStore = ds.cloneWithRows(this.props.employees)
-     }
+    this.createDataSource(nextProps);
+  }
 
-     renderRow(employee){
-         return <ListItem employee={employee} />
-     }
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
-     render(){
-         console.log(this.props)
-         return(
-             <Text>abc</Text>
-            //  <ListView
-            //     enableEmptySections
-            //     dataSource={this.dataSource}
-            //     renderRow={this.renderRow}
-            //  />
-         )
-     }
- }
+    this.dataSource = ds.cloneWithRows(employees);
+  }
 
- const mapStateToProps = state => {
-     const employees = _.map(state.employees, (val,uid) => {
-         return {...val, uid }
-     })
-     return { employees }
- }
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
 
- export default  connect(mapStateToProps, {employeesFetch})(EmployeeList)
+  render() {
+    return (
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
