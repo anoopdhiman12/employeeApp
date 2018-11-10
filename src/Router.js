ca7c15a8509c2b42a36b5c9882b2b6013094a467
Anoop <anoop.dhiman12@gmail.com>
2018-11-10 14:59:31 +0530

    editing employee and saving to firebase

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
-// import EmployeeEdit from './components/EmployeeEdit';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -18,6 +18,7 @@ const RouterComponent = () => {
         onRight={() => { Actions.employeeCreate() }}
         key="employeeList" component={EmployeeList} title="Employee List" initial />
         <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee"/>
+        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
         </Scene>
         </Scene>
 
@@ -34,7 +35,7 @@ const RouterComponent = () => {
           initial
         />
         <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
-        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
+        
       </Scene> */}
     </Router>
   );
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,68 +1,36 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Card, CardSection, Input, Button } from './common'
-import { Picker, Text } from 'react-native'
-
-import { employeeUpdate, employeeCreate } from '../actions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate, employeeCreate } from '../actions';
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
 
-    onButtonPress() {
-        const { name, phone, shift } = this.props;
-        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
-      }
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
 
-    render() {
-        return(
-            <Card>
-                <CardSection>
-                    <Input 
-                    label="Employee Name" 
-                    placeholder="jane" 
-                    value={this.props.name} 
-                    onChangeText={value => this.props.employeeUpdate({prop:'name',value})}/>
-                </CardSection>
-                <CardSection>
-                <Input label="Phone Number" placeholder="123123123" value={this.props.phone}
-                onChangeText={ value => this.props.employeeUpdate({prop:'phone',value})}/>
-                </CardSection>
-                <CardSection >
-                    <Text style={styles.pickerTextStyle}>Shift</Text>
-                    <Picker
-                        selectedValue={this.props.shift}
-                        onValueChange={value => this.props.employeeUpdate({ props: 'shift', value})}
-                        style={{flex:1}}
-                    >
-                        <Picker.Item label="Monday" value="Monday"/>
-                        <Picker.Item label="Tuesday" value="Tuesday"/>
-                        <Picker.Item label="Wednesday" value="Wednesday"/>
-                        <Picker.Item label="Thursday" value="Thursday"/>
-                        <Picker.Item label="Friday" value="Friday"/>
-                        <Picker.Item label="Saturday" value="Saturday"/>
-                        <Picker.Item label="Sunday" value="Sunday"/>
-
-                    </Picker>
-                </CardSection>
-                <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Create
-                    </Button>
-                </CardSection>
-            </Card>
-        )
-    }
-}
-
-const styles =  {
-    pickerTextStyle: {
-        fontSize: 18,
-        paddingLeft: 20
-    }
+  render() {
+    return (
+      <Card>
+        <EmployeeForm {...this.props} />
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    const {name, phone, shift}  = state.employeeForm
-    return { name, phone, shift }
-}
+  const { name, phone, shift } = state.employeeForm;
 
-export default connect(mapStateToProps, { employeeUpdate,employeeCreate})(EmployeeCreate)
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeCreate
+})(EmployeeCreate);
